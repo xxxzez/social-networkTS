081d2a052dda5cf745d4badde986f52b7971967d
Matvei Bohush <bohush.mathew@gmail.com>
2020-10-16 03:46:42 +0300

    feat: add basic structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">News Feed</a>
+            <a href="#s">Messages</a>
+        </div>
+    );
+}
+
+export default Header;
