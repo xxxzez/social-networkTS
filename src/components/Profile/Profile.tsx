d7a82a52e9c1f67bda310b26c6b15f58a394aa90
Matvei Bohush <bohush.mathew@gmail.com>
2020-10-18 01:24:27 +0300

    ref: style navbar

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import styles from "./Profile.module.css";
-import logo from "../../assets/stadium.jpeg";
+import logo from "../../assets/profilePicture.jpeg";
 
 const Profile = () => {
     return (
-        <div>
-            <div>
-                <img src={logo} alt="" width="900px" />
+        <div className={styles.profile}>
+            <div className={styles.profilePicture}>
+                <img src={logo} alt="" />
             </div>
-            <div>The most gorgeous tournament in CSGO history!</div>
+            <div>Welcome to my page guys! Happy to see you here!</div>
             <MyPosts />
         </div>
     );
-}
+};
 
 export default Profile;
