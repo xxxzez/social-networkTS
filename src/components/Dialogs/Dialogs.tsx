a3ff4c4275179b4acbfccb0dd662843ba80fb197
Matvei Bohush <bohush.mathew@gmail.com>
2020-10-18 06:10:04 +0300

    ref: map dialogs and posts

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -12,7 +12,7 @@ type MessageType = {
     message: string
 }
 
-const DialogsData: Array<DialogType> = [
+const dialogs: Array<DialogType> = [
     { id: 1, name: "Юля" },
     { id: 2, name: "Ника" },
     { id: 3, name: "Влад" },
@@ -20,14 +20,13 @@ const DialogsData: Array<DialogType> = [
     { id: 5, name: "Серега" },
 ]
 
-const MessagesData: Array<MessageType> = [
+const messages: Array<MessageType> = [
     { id: 1, message: "Ну шо ты там" },
     { id: 2, message: "Просто здравствуй" },
     { id: 3, message: "Просто как дела" },
     { id: 4, message: "Где встретимся?" },
     { id: 5, message: "Всё ясно, пока" },
 ]
-
 const DialogItem: React.FC<DialogType> = (props) => {
     return (
         <div className={styles.dialog}>
@@ -39,39 +38,18 @@ const DialogItem: React.FC<DialogType> = (props) => {
 const Message: React.FC<MessageType> = (props) => {
     return <div className={styles.message}>{props.message}</div>
 }
+const dialogsElements = dialogs.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+))
+const messagesElements = messages.map((m) => (
+    <Message message={m.message} id={m.id} />
+))
 
 const Dialogs = () => {
     return (
         <div className={styles.dialogs}>
-            <div className={styles.dialogsItems}>
-                <DialogItem name={DialogsData[0].name} id={DialogsData[0].id} />
-                <DialogItem name={DialogsData[1].name} id={DialogsData[1].id} />
-                <DialogItem name={DialogsData[2].name} id={DialogsData[2].id} />
-                <DialogItem name={DialogsData[3].name} id={DialogsData[3].id} />
-                <DialogItem name={DialogsData[4].name} id={DialogsData[4].id} />
-            </div>
-            <div className={styles.messages}>
-                <Message
-                    message={MessagesData[0].message}
-                    id={MessagesData[0].id}
-                />
-                <Message
-                    message={MessagesData[1].message}
-                    id={MessagesData[1].id}
-                />
-                <Message
-                    message={MessagesData[2].message}
-                    id={MessagesData[2].id}
-                />
-                <Message
-                    message={MessagesData[3].message}
-                    id={MessagesData[3].id}
-                />
-                <Message
-                    message={MessagesData[4].message}
-                    id={MessagesData[4].id}
-                />
-            </div>
+            <div className={styles.dialogsItems}>{dialogsElements}</div>
+            <div className={styles.messages}>{messagesElements}</div>
         </div>
     )
 }
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,24 @@ import React from "react"
 import Post from "./Post/Post"
 import styles from "./MyPosts.module.css"
 
+type PostType = {
+    id: number
+    message: string
+    likesCount: number
+}
+
+const posts: Array<PostType> = [
+    { id: 1, message: "First post!!!!", likesCount: 17 },
+    { id: 2, message: "Some bullshit", likesCount: 7 },
+    { id: 3, message: "Nothing special", likesCount: 11 },
+    { id: 4, message: "Im fine", likesCount: 77 },
+    { id: 5, message: "qq", likesCount: 71 },
+]
+
+const postsElements = posts.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} />
+))
+
 const MyPosts = () => {
     return (
         <div>
@@ -15,10 +33,7 @@ const MyPosts = () => {
                     <button>Delete post</button>
                 </div>
             </div>
-            <div className={styles.posts}>
-                <Post message="First post!!!!" likesCount={17} />
-                <Post message="Some bullshit" likesCount={6} />
-            </div>
+            <div className={styles.posts}>{postsElements}</div>
         </div>
     )
 }
