64fb39711710bac54a1cff6b401e2e7b9f518919
Matvei Bohush <bohush.mathew@gmail.com>
2020-10-20 04:43:08 +0300

    style: news, settings components

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -3,7 +3,7 @@ import s from './Settings.module.css'
 
 const Settings = () => {
     return (
-        <div className={s.Settings}>
+        <div className={s.settings}>
             Settings
         </div>
     );
