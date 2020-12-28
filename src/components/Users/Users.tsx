4be758d05b5f68c3c5e5b9b2c6624ebe78624275
Matvei Bohush <bohush.mathew@gmail.com>
2020-12-28 23:57:34 +0300

    add PropsType for class component Users

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,60 +6,72 @@ import profileIcon from '../../assets/profileIcon.png'
 import locationLogo from '../../assets/canadaFlag.jpeg'
 
 type PropsType = UsersPropsFromRedux
-export const Users: React.FC<PropsType> = React.memo((props) => {
-    if (props.usersPage.users.length === 0) {
-        axios
-            .get('https://social-network.samuraijs.com/api/1.0/users')
-            .then((response) => {
-                props.setUsers(response.data.items)
-            })
+
+export class Users extends React.Component<PropsType> {
+    getUsers = () => {
+        if (this.props.usersPage.users.length === 0) {
+            axios
+                .get('https://social-network.samuraijs.com/api/1.0/users')
+                .then((response) => {
+                    this.props.setUsers(response.data.items)
+                })
+        }
     }
-    return (
-        <div>
-            {props.usersPage.users.map((u) => (
-                <div className="card" key={u.id}>
-                    <img
-                        src={
-                            u.photos.small !== null
-                                ? u.photos.small
-                                : profileIcon
-                        }
-                        alt="ProfileImage"
-                        width="90px"
-                    />
+    render = () => {
+        return (
+            <div>
+                <Button
+                    onClick={this.getUsers}
+                    variant="contained"
+                    color="primary"
+                >
+                    Get users
+                </Button>
+                {this.props.usersPage.users.map((u) => (
+                    <div className="card" key={u.id}>
+                        <img
+                            src={
+                                u.photos.small !== null
+                                    ? u.photos.small
+                                    : profileIcon
+                            }
+                            alt="ProfileImage"
+                            width="90px"
+                        />
 
-                    <h3>{u.name}</h3>
-                    <h4>{u.status}</h4>
-                    <h4>
-                        {'Vancouver'}, {'Canada '}
-                        <img src={locationLogo} alt="" width="30px" />
-                    </h4>
+                        <h3>{u.name}</h3>
+                        <h4>{u.status}</h4>
+                        <h4>
+                            {'Vancouver'}, {'Canada '}
+                            <img src={locationLogo} alt="" width="30px" />
+                        </h4>
 
-                    <p>
-                        {u.followed ? (
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                onClick={() => {
-                                    props.follow(u.id)
-                                }}
-                            >
-                                Unfollow
-                            </Button>
-                        ) : (
-                            <Button
-                                variant="text"
-                                color="primary"
-                                onClick={() => {
-                                    props.unfollow(u.id)
-                                }}
-                            >
-                                Follow
-                            </Button>
-                        )}
-                    </p>
-                </div>
-            ))}
-        </div>
-    )
-})
+                        <p>
+                            {u.followed ? (
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={() => {
+                                        this.props.follow(u.id)
+                                    }}
+                                >
+                                    Unfollow
+                                </Button>
+                            ) : (
+                                <Button
+                                    variant="text"
+                                    color="primary"
+                                    onClick={() => {
+                                        this.props.unfollow(u.id)
+                                    }}
+                                >
+                                    Follow
+                                </Button>
+                            )}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
