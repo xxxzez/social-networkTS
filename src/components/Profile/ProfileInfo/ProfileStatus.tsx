8613d2529a5e52696c3e26c11f13379cf28990b6
Matvei Bohush <bohush.mathew@gmail.com>
2021-02-03 21:45:46 +0300

    profile status with useEffect

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 
 type PropsType = {
     status: string
@@ -8,6 +8,9 @@ type PropsType = {
 export const ProfileStatus = (props: PropsType) => {
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
 
     const activateEditMode = () => {
         setEditMode(true)
