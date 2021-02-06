825ec726a7fa1669c147f18e6c53d389cdb50738
Matvei Bohush <bohush.mathew@gmail.com>
2021-02-06 21:01:54 +0300

    add 404 error to routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { Navbar } from './components/Navbar/Navbar'
 import { Settings } from './components/Settings/Settings'
-import { Route, withRouter } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 import { News } from './components/News/News'
 import { HeaderContainer } from './components/Header/HeaderContainer'
 import { Grid } from '@material-ui/core'
@@ -36,24 +36,33 @@ export class SimpleApp extends React.Component<any> {
                 <Grid container>
                     <Navbar />
                     <Grid item xs={10}>
-                        <Route
-                            path={'/profile/:userId?'}
-                            render={withSuspense(ProfileContainer)}
-                        />
-                        <Route
-                            path={'/messages'}
-                            render={withSuspense(DialogsContainer)}
-                        />
-                        <Route
-                            path={'/users'}
-                            render={() => <UsersContainer />}
-                        />
-                        <Route path={'/news'} render={() => <News />} />
-                        <Route path={'/settings'} render={() => <Settings />} />
-                        <Route
-                            path={'/login'}
-                            render={() => <ConnectedLogin />}
-                        />
+                        <Switch>
+                            <Route
+                                path={'/profile/:userId?'}
+                                render={withSuspense(ProfileContainer)}
+                            />
+                            <Route
+                                path={'/messages'}
+                                render={withSuspense(DialogsContainer)}
+                            />
+                            <Route
+                                path={'/users'}
+                                render={() => <UsersContainer />}
+                            />
+                            <Route path={'/news'} render={() => <News />} />
+                            <Route
+                                path={'/settings'}
+                                render={() => <Settings />}
+                            />
+                            <Route
+                                path={'/login'}
+                                render={() => <ConnectedLogin />}
+                            />
+                            <Route
+                                path={'*'}
+                                render={() => <div>404 NOT FOUND</div>}
+                            />
+                        </Switch>
                     </Grid>
                 </Grid>
             </div>
