ac6041a4e5e2afed85c9f435c72c894041a8053b
Matvei Bohush <bohush.mathew@gmail.com>
2021-02-18 22:28:09 +0300

    ref

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -18,10 +18,13 @@ const rootReducer = combineReducers({
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
+
 type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never
+
 export type InferActionsTypes<
     T extends { [key: string]: (...args: any[]) => any }
 > = ReturnType<PropertiesTypes<T>>
+
 export type BaseThunkType<A extends Action, R = Promise<void>> = ThunkAction<
     R,
     AppStateType,
