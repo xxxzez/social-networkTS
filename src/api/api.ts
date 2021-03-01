eca3c1fe9121861342248f2e34da6d761045bf32
Matvei <47398536+xxxzez@users.noreply.github.com>
2021-03-01 23:35:06 +0300

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,18 +8,22 @@ export const instance = axios.create({
         'API-KEY': '09428775-ee37-4314-b1ba-564d5848e89a',
     },
 })
+
 export enum ResultCodesEnum {
     Success = 0,
     Error = 1,
 }
+
 export enum ResultCodeForCaptchaEnum {
     CaptchaIsRequired = 10,
 }
+
 export type GetItemsType = {
     items: UserType[]
     totalCount: number
     error: string | null
 }
+
 export type APIResponseType<D = {}, RC = ResultCodesEnum> = {
     data: D
     messages: string[]
